35a736af9c4d8b8610f3e9601eb0d16fe3ba67cd
Jefferson Leon <jefferson.leon@usantotomas.edu.co>
2020-08-04 23:38:07 -0500

    task 4

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -2,9 +2,9 @@
       file_Line { 'Turn off passwd auth':
       ensure => 'present',
       path   => '/etc/ssh/ssh_config',
-      line   => '    PasswordAutentication no'
+      line   => '    PasswordAuthentication no'
       }
-      file_line { 'Declare identy file':
+      file_line { 'Declare identity file':
       ensure => 'present',
       path   => '/etc/ssh/ssh_config',
       line   => '    IdentityFile ~/.ssh/holberton'
