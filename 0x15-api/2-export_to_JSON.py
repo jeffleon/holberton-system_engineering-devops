cd146914a7c7ea9d18f3df844c9f41e151e5f0c1
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-09-15 04:53:57 +0000

    [UPDATE] task 2

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -3,7 +3,7 @@
 import json
 import requests
 from sys import argv
- 
+
 
 if __name__ == "__main__":
     """ Print Tasks """
@@ -25,15 +25,8 @@ if __name__ == "__main__":
     json_dict = {}
     for task in Tasks:
         TasksJSON.append({'task': task['title'],
-                           'completed': task['completed'],
-                           'username': name
-                           })
+                          'completed': task['completed'],
+                          'username': name})
     json_dict = {argv[1]: TasksJSON}
-    data = {
-    "president": {
-        "name": "Zaphod Beeblebrox",
-        "species": "Betelgeusian"
-        }
-    }
     with open('{}.json'.format(argv[1]), mode="w", encoding="utf-8") as fd:
-        json.dump([json_dict], fd)
+        json.dump(json_dict, fd)
