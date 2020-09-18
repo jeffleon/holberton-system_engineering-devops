e104b7fdaf824c48c68441b77d9e58ec8673c9df
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-09-18 00:12:28 -0200

    add task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,13 +5,12 @@ import requests
 
 def top_ten(subreddit):
     """ function that allow do a request in reddit to obtein titles """
-    headers = {'User-Agent': 'Mozilla/5.0', "exact": True}
+    headers = {'User-Agent': 'Mozilla/5.0'}
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    try:
-        response = requests.get(url, headers=headers,
-                                data=data, allow_redirects=False)
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
         results = response.json()
         for result in results["data"]["children"]:
             print(result['data']['title'])
-    except(e):
+    else:
         print(None)
