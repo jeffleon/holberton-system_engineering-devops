44d811745977d6c8a0d2eedb5acbc98acbf56299
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-09-17 23:57:14 -0200

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,11 +7,11 @@ def number_of_subscribers(subreddit):
     """ function to find a subredict """
     url = "https://www.reddit.com/api/search_subreddits.json"
     headers = {'User-Agent': 'Mozilla/5.0'}
-    data = {"query": subreddit}
-    response = requests.post(url, headers=headers, data=data)
+    data = {"query": subreddit, "exact": True}
+    response = requests.post(url, headers=headers,
+                             data=data, allow_redirects=False)
     if response.status_code == 200:
         results = response.json()
         return results['subreddits'][0]['subscriber_count']
     else:
         return 0
-
