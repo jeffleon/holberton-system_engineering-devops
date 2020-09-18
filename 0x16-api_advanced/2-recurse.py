364cbcd977af71ffb822e4d96b8fcd4bc6528f1d
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-09-18 00:33:20 -0200

    add task 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -8,16 +8,16 @@ def recursive(after, sub, hot_list=[]):
     """" Go throw all pages """
     headers = {'User-Agent': 'Mozilla/5.0'}
     url = "https://www.reddit.com/r/{}/hot.json?after={}".format(sub, after)
-    try:
-        response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
         results = response.json()
         after = results['data']['after']
         for result in results["data"]["children"]:
             hot_list.append(result['data']['title'])
         if after is None:
             return hot_list
-        recursive(after, sub, hot_list)
-    except(e):
+        return recursive(after, sub, hot_list)
+    else:
         return None
 
 
@@ -25,4 +25,5 @@ def recurse(subreddit, hot_list=[]):
     """" Aux function"""
     after = ""
     sub = subreddit
-    recursive(after, sub, hot_list)
+    hot_list = recursive(after, sub, hot_list)
+    return hot_list
