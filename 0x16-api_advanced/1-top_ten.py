9189c6b5e5630b92bb0d8ab36b94ee4145ce2569
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-09-18 00:05:37 -0200

    1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,10 +5,11 @@ import requests
 
 def top_ten(subreddit):
     """ function that allow do a request in reddit to obtein titles """
-    headers = {'User-Agent': 'Mozilla/5.0'}
+    headers = {'User-Agent': 'Mozilla/5.0', "exact": True}
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     try:
-        response = requests.get(url, headers=headers)
+        response = requests.get(url, headers=headers,
+                                data=data, allow_redirects=False)
         results = response.json()
         for result in results["data"]["children"]:
             print(result['data']['title'])
